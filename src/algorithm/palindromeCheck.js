c4f2e02ba97d7b22a7e15ae8d3b124581ed2a4c0
Brian <brianfounder@hotmail.com>
2017-07-22 15:30:05 +0800

    add palindromeCheck solution 3 reverse the 2nd half of the list, space O(1) using 3 variables

--- src/algorithm/palindromeCheck.js
+++ src/algorithm/palindromeCheck.js
@@ -28,7 +28,7 @@ var isPalindrome2 = function (head) {
 
     var p = head;
     var rightHead = head;
-    while (p.next && p.next.next) {
+    while (p.next && p.next.next) { // two pointers, one moves 2x faster than the other
         p = p.next.next;
         rightHead = rightHead.next
     }
@@ -49,6 +49,48 @@ var isPalindrome2 = function (head) {
 
 };
 
+var isPalindrome3 = function (head) {
+    if (head == null || head.next == null) return head;
+
+    var p = head;
+    var rightHead = head;
+    while (p.next && p.next.next) {
+        p = p.next.next;
+        rightHead = rightHead.next
+    }
+    var pre = rightHead;
+    rightHead = rightHead.next;
+    pre.next = null;
+
+    while (rightHead) {
+        p = rightHead.next;
+        rightHead.next = pre;
+        pre = rightHead;
+        rightHead = p
+    }
+
+    p = head;
+    rightHead = pre;
+    while (pre != null && p != null) {
+        if (pre.value != p.value) {
+            return false
+        }
+        pre = pre.next;
+        p = p.next
+    }
+
+    pre = null;
+    while (rightHead) { // restore the list
+        p = rightHead.next;
+        rightHead.next = pre;
+        pre = rightHead;
+        rightHead = p
+    }
+
+    return true
+
+};
+
 export function palindromeCheck() {
     var test1 = function () {
         var head = new Node(1);
@@ -58,5 +100,5 @@ export function palindromeCheck() {
         head.next.next.next.next = new Node(1);
         return head
     }();
-    return isPalindrome2(test1)
+    return isPalindrome3(test1)
 }
